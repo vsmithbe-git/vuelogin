37b60190925d7e396a722fbd2a4e25d24928bff5
vsmithbe-git <vsmithbe.git@gmail.com>
2022-03-19 02:31:39 -0500

    updated dependencies

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,15 +13,15 @@ const loggingOut = () => {
 </script>
 
 <template>
-<div class="bg-indigo-800 text-indigo-200">	
+<div class="bg-yellow-800 text-yellow-200">	
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="tracking-tight text-3xl font-thin">Vue<span class="font-normal">FakeAuth</span>
         </h1>
         <nav>
             <ul class="flex space-x-4 w-full">
-                <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">Home</li></router-link>
-                <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">About</li></router-link>
-                <router-link v-if="!isAuthenticated" to="/login"><li class="py-8 px-4 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">Login</li></router-link>
+                <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-500 hover:text-yellow-800">Home</li></router-link>
+                <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-500 hover:text-yellow-800">About</li></router-link>
+                <router-link v-if="!isAuthenticated" to="/login"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-500 hover:text-yellow-800">Login</li></router-link>
                 <div v-else class="flex">
                  <router-link  :to="{name: 'Secret'}"><li class="py-8 px-4 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">Secret</li></router-link>
                 <button  @click="loggingOut"><li class="py-8 px-4 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">Logout</li>
