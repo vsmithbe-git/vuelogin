03ba5fe45478f1b123e6a8970e0f42139e8f2615
vsmithbe-git <vsmithbe.git@gmail.com>
2022-03-28 02:24:36 -0500

    Add Validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,14 +1,31 @@
 <script setup>
+
 import {ref} from "vue";
 import {useRouter} from "vue-router";
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
+
 import useAuth from '../composable/useAuth';
 import useError from "../composable/useError";
 import { useTimeout, promiseTimeout } from '@vueuse/core';
 
+const schema = yup.object({
+      username: yup.string().required().email().label("Email"),
+      password: yup.string().required().min(8).label("Password"),
+    });
+
+     useForm({
+      validationSchema: schema,
+    });
+
+const { value: username, errorMessage: emailError } = useField('username');
+const { value: password, errorMessage: passwordError } = useField('password');
+
+
 const {isAuthenticated,  login, signup, googleLogin} = useAuth();
 
-const username = ref("");
-const password = ref("");
+//const username = ref("");
+//const password = ref("");
 
 const router = useRouter();
 
@@ -52,13 +69,28 @@ const { ready, start } = useTimeout(3000, { controls: true }, false);
     
       <img class = "h-64" src="../assets/bglogin.png" alt="Login BG">
   <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
-    <input type="text" class="border-2 p-2 rounded-lg" placeholder="Username" v-model = "username"/>
-    <input type="password" class="border-2 p-2 rounded-lg" placeholder="Password" v-model = "password"/>
+    <input 
+      name="username"
+      type="text" 
+      class="border-2 p-2 rounded-lg"
+      placeholder="Email" 
+      v-model = "username"
+      />
+  <span class="text-red-500 text-center text-xs">{{emailError}}</span>
+    <input 
+      name="password"
+      type="password" 
+      class="border-2 p-2 rounded-lg" 
+      placeholder="Password" 
+      v-model = "password"
+      />
+      <span class="text-red-500 text-center text-xs">{{passwordError}}</span>
     <div class="flex space-x-2">
     <button type="submit" @submit.prevent="logginIn" class="w-1/2 bg-yellow-600 text-yellow-200 py-2 rounded-lg">Login</button>
      <button  
      @click ="signingUp" 
      class="w-1/2 bg-green-600 text-green-200 py-2 rounded-lg">SignUp</button>
+      
     </div>
     
     <button @click="google"
